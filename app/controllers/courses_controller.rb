63634c92c658be102d3043603f7adb7510d605d7
Kris Mora <kmora3@yahoo.com>
2017-01-03 04:12:08 -0800

    Create course, log in/out forms

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,22 @@
    class CoursesController < ApplicationController
      def index
      end
-   end
+
+     def new
+       @course = Course.new
+     end
+
+     def create
+       @course = Course.new(course_params)
+       @course.save
+       redirect_to @course
+     end
+
+     def show
+     end
+
+    private
+      def course_params
+        params.require(:course).permit(:name, :description, :address, :city, :state, :zipcode, :phone, :phone, :hole_count)
+      end
+    end
